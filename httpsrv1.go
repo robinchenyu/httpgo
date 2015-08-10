9c0ba6c75da6623b7a450906304b7e102a43c8f6
ChenYu <robin.chenyu@gmail.com>
2015-08-10 14:24:25 +0000

    add flag for password and port args

--- httpsrv1.go
+++ httpsrv1.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"html/template"
 	"log"
@@ -8,13 +9,50 @@ import (
 	"strings"
 )
 
+var inch chan string = make(chan string)
+var outch chan string = make(chan string)
+var password string
+var httpPort string
+
+func init() {
+	flag.StringVar(&password, "password", "mypassword", "The password for users")
+	flag.StringVar(&httpPort, "port", "9999", "The http listen port")
+}
+
 func main() {
+	flag.Parse()
 	http.HandleFunc("/", sayHelloName)
 	http.HandleFunc("/login", login)
-	fmt.Println("Listen on :9090")
-	err := http.ListenAndServe(":9090", nil)
+	http.HandleFunc("/updateIp", updateIp)
+	http.HandleFunc("/getIp", getIp)
+	go cacheIp(inch, outch)
+	fmt.Println("Listen on :", httpPort)
+	err := http.ListenAndServe(":"+httpPort, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe err: ", err)
+	}
+}
+
+func cacheIp(in chan string, out chan string) {
+	ips := make(map[string]string)
+	for {
+		select {
+		case x := <-in:
+			data := strings.Split(x, ":")
+			if data[0] == "update" {
+				ips[data[1]] = data[2]
+				fmt.Println("update ip", data[1], data[2])
+			}
+			if data[0] == "get" {
+				ip2, ok := ips[data[1]]
+				fmt.Println("get ip", data[1], ip2)
+				if ok {
+					out <- ip2
+				} else {
+					out <- "127.0.0.1"
+				}
+			}
+		}
 	}
 }
 
@@ -37,6 +75,33 @@ func login(w http.ResponseWriter, r *http.Request) {
 	} else {
 		r.ParseForm()
 		fmt.Println("username: ", r.Form["username"])
+		fmt.Println("username.len: ", r.Form["username"][0])
 		fmt.Println("password: ", r.Form["password"])
+	}
+}
+
+func updateIp(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	user, pass, ok := r.BasicAuth()
+	if ok {
+		if user[:5] == "robin" && pass == password {
+			rip := strings.Split(r.RemoteAddr, ":")[0]
+			fmt.Println("remoteAddr", rip)
+			inch <- "update:" + user + ":" + rip
+		}
+	}
+}
+
+func getIp(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	user, pass, ok := r.BasicAuth()
+	if ok {
+		if user[:5] == "robin" && pass == password {
+			rip := strings.Split(r.RemoteAddr, ":")[0]
+			fmt.Println("remoteAddr", rip)
+			inch <- "get:" + user + ":127.0.0.1"
+			w.Write([]byte(<-outch))
+
+		}
 	}
 }
